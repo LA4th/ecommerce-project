84f9aba1c5e75aee110c0cf4fef30818a883170c
Enzo Laforteza <lorenzolaforteza311@gmail.com>
2025-11-18 21:16:03 +0800

    Adding the list but no responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import TheHeader from './components/TheHeader'
+import TheHeader from './components/header/TheHeader'
+import TheFeaturedProducts from './components/featuredProducts/TheFeaturedProducts'
+import DataMedicine from './components/data/DataMedicine.json'
 
 function App() {
-  
+  const [itemsList, setItemsList] = useState(DataMedicine);
 
   return (
     <>
-    <TheHeader />
+      {/* <TheHeader /> */}
+      <TheFeaturedProducts itemsList={itemsList} />
     </>
   )
 }
--- src/components/featuredProducts/TheFeaturedProducts.jsx
+++ src/components/featuredProducts/TheFeaturedProducts.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import TheCard from "./TheCard";
 
-export default function TheFeaturedProducts () {
+export default function TheFeaturedProducts ({ itemsList }) {
 
   return (
     <>
+      <div>
+        <h1>Common Over-the-Counter Products</h1>
+        <p>Always read the label and consult a pharmacist if unsure.</p>
+      </div>
+      {/* CARD RENDER */}
+
+      {/* CARD CONTAINER */}
+      <TheCard itemList={ itemsList } />
     </>
   );
 };
